64f6e3d8fc2f770a0acd269f74f73477c40d54fb
Sanoj Chathuranga Amarasiri <chathuranga.amarasiri91@gmail.com>
2020-12-04 21:47:14 +0530

    navbar edit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React,{ useState } from 'react';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -14,25 +14,25 @@ function Navbar() {
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links nav-active' onClick={closeMobileMenue}>
-                            <i class="fas fa-home"/> HOME
-                        </Link>
+                    <li>
+                        <NavLink to='/' exact className='nav-links' activeClassName='nav-active' onClick={closeMobileMenue}>
+                            <i class="fas fa-home fa-fw"/> Home
+                        </NavLink>
                     </li>
-                    <li className='nav-item'>
-                        <Link to='/about' className='nav-links' onClick={closeMobileMenue}>
-                            <i class="fas fa-address-card"/> About
-                        </Link>
+                    <li>
+                        <NavLink to='/about' className='nav-links' activeClassName='nav-active' onClick={closeMobileMenue}>
+                            <i class="fas fa-address-card fa-fw"/> About
+                        </NavLink>
                     </li>
-                    <li className='nav-item'>
-                        <Link to='/projects' className='nav-links' onClick={closeMobileMenue}>
-                            <i class="fas fa-briefcase"/> Projects
-                        </Link>
+                    <li>
+                        <NavLink to='/projects' className='nav-links' activeClassName='nav-active' onClick={closeMobileMenue}>
+                            <i class="fas fa-briefcase fa-fw"/> Projects
+                        </NavLink>
                     </li>
-                    <li className='nav-item'>
-                        <Link to='/contact' className='nav-links' onClick={closeMobileMenue}>
-                            <i class="fas fa-address-book"/> Contact
-                        </Link>
+                    <li>
+                        <NavLink to='/contact' className='nav-links' activeClassName='nav-active' onClick={closeMobileMenue}>
+                            <i class="fas fa-address-book fa-fw"/>Contact
+                        </NavLink>
                     </li>
 
                 </ul>
